3d4df09eb18be7149ac768f0596e0e37fb601db7
WdataW <wael272726@gmail.com>
2025-03-16 21:43:27 +0300

    Add displayCurrentWeather createPropertyNode functions and update app.js for weather display
    functions were created in 'ui.js' to display the current weather properties. NOTE: they are temporary and may be modified/deleted later.

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,24 @@
 import { fetchLocations } from "./location.js";
 import { displayLocations } from "./ui.js"
 import { fetchWeather } from "./weather.js"
+import { displayCurrentWeather } from "./ui.js";
 const getLocations = document.querySelector(".fetch-location");
 const locationField = document.querySelector(".specify-location");
 
-/* after writing the location name and clicking the button: */
+// after writing the location name and clicking the button: 
 getLocations.addEventListener("click", async function () {
   try {
     const locationName=locationField.value;
-    if(locationName.trim()){/* to check that the locationField isn't empty */
-      const locations = await fetchLocations(locationName);
-      const locationOptions= displayLocations(locations);
-      let chosenLocation;
+    if(locationName.trim()){// to check that 'locationField' value isn't empty.
+      const locations = await fetchLocations(locationName); // getting the locations matching 'locationName'.
+      const locationOptions= displayLocations(locations); // displaying the locations to the user. LocationOptions refers to the container.
       locationOptions.addEventListener("click", async function (event) {
         try{
           if(event.target.tagName==="BUTTON"){
-            const indexOfLocation=Number(event.target.id);/* refers to the index in the locations array */
-            chosenLocation= locations[indexOfLocation];
-            const weatherData=await fetchWeather(chosenLocation);
+            const indexOfLocation=Number(event.target.id);// refers to the index in 'locations' array.
+            const chosenLocation= locations[indexOfLocation];
+            const weatherData=await fetchWeather(chosenLocation);// getting the weather data for the chosen location.
+            displayCurrentWeather(weatherData["current"]); //diplaying the 'current weather' properties. NOTE: it will be changed later in the design stage.
           }
         }catch(error){
           console.error(error);
--- scripts/ui.js
+++ scripts/ui.js
@@ -18,3 +18,31 @@ export function displayLocations(locationsArray){
 }
 
 
+/* 
+* displayCurrentWeather displays the properties of the current weather.
+* @param {object} currentWeather - it contains the current weather data. 
+* @returns {void}.
+*/
+export function displayCurrentWeather(currentWeather){
+    const weatherContainer=document.createElement("div");
+    const properties=["temperature_2m","apparent_temperature","wind_speed_10m","wind_direction_10m","relative_humidity_2m"];
+    
+    for(const property of properties){
+        const propertyNode=createPropertyNode(currentWeather,property)
+        weatherContainer.appendChild(propertyNode);
+    }
+    document.body.appendChild(weatherContainer);
+}
+
+
+/* 
+* createPropertyNode creates a <p> element then sets its textContent to the property's key and value.
+* @param {object} object - represents the object that holds the targeted property.
+* @param {string} property - represents the key of the property.
+* @returns {object} propertyNode - it is the <p> element that holds the text describing the property.
+*/
+function createPropertyNode(object,property){
+    const propertyNode= document.createElement("p");
+    propertyNode.textContent=`${property}: ${object[property]}`;
+    return propertyNode;
+}
