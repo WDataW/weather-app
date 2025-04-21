520ae758ebb018558e77e7b6e261a2da9822c6db
WdataW <wael272726@gmail.com>
2025-04-21 21:47:04 +0300

    Added updateWeather feature
    updates the weather data every 15 minutes

--- scripts/app.js
+++ scripts/app.js
@@ -10,6 +10,8 @@ async function initWeather() {
   try {
     const defaultWeather = await getDefaultWeather();
     generatePage(defaultWeather);
+
+    startUpdatingWeather(defaultWeather);
   } catch (error) {
     console.error(error);
   }
@@ -26,4 +28,36 @@ export async function displayNewWeather(location) {
   const weather = await getWeather(location);
   erasePageContents();
   generatePage(weather);
+
+  startUpdatingWeather(weather);
+}
+
+/*
+ * updateWeather updates the page contents with new weather data but using the same location as the previous weather
+ * @param {object} previousWeather - contains contains weather data based on a specific location
+ * @returns {void}
+ */
+import {msTillNextUpdate} from "./time.js";
+async function updateWeather(previousWeather) {
+  const location = {
+    lat: previousWeather["latitude"],
+    lon: previousWeather["longitude"],
+  };
+  const weather = await getWeather(location);
+  weather["locationName"] = previousWeather["locationName"];
+  erasePageContents();
+  generatePage(weather);
+}
+
+/*
+ * startUpdatingWeather initiates the interval that updates the weather every 15 minutes
+ * @param {object} previousWeather - contains contains weather data based on a specific location
+ * @returns {void}
+ */
+let updateInterval;
+function startUpdatingWeather(previousWeather) {
+  clearInterval(updateInterval);
+  updateInterval = setInterval(() => {
+    updateWeather(previousWeather);
+  }, msTillNextUpdate());
 }
